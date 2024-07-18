b972c8eb919c30a610afe82caf965ce04b5819f1
Admin <test@test.com>
2024-07-18 16:15:38 +0300

    new file:   main.py, index.html

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request, redirect,flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField,SubmitField, EmailField, TelField, BooleanField
+from wtforms.validators import Email, Length, DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'PleaseChageMe'
+
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/home', methods=['POST', 'GET'])
+def main():
+    return render_template('index.html')
